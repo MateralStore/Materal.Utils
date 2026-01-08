4e6d95acee59e753acfd9f8af502a53b8254c6af
Materal <cloomcmx1554@hotmail.com>
2026-01-08 09:05:52 +0800

    feat: 添加控制台测试项目

---
+++ Materal.Utils.ConsoleTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Materal.Utils.ConsoleTest;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        ConsoleQueue.WriteLine("测试结束，按任意键退出", ConsoleColor.Yellow);
+        await ConsoleQueue.ShutdownAsync();
+        Console.ReadKey();
+    }
+}
