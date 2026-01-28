c3e7930ecc35defb7afd9827a43d6481e236d933
Materal <cloomcmx1554@hotmail.com>
2026-01-28 11:20:30 +0800

    feat: 更新测试项目

--- Materal.Utils.Test/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-// 启用测试并行化以提高测试执行性能
-// Workers = 0 表示使用所有可用的处理器核心
-// Scope = MethodLevel 表示在方法级别并行执行测试
-[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
